38e2f49a02785512ccaf586ecd8478ff8e5f9dbe
dandybudiman <dandybudiman9@gmail.com>
2024-09-27 09:55:14 +0700

    second_commit

--- IMA_Latihan1/Main.java
+++ IMA_Latihan1/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello1");
+        System.out.println("lagi");
+        System.out.println("walaweh");
+        System.out.println("haii");
+        System.out.println("hmm");
     }
 }
